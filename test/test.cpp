e66d5cefc7192f6a212664cbe4006b3bc8ec8bdf
theo <theobfernandez@gmail.com>
2022-11-13 19:14:32 -0800

    Add tests for generateMoviesVector()

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,62 @@
 #include "gtest/gtest.h"
 #include "../include/Movie.h"
 #include "../include/Movies.h"
+#include "../include/DatasetAccessor.h"
+#include "iostream"
 
+TEST(MoviesVectorSetupTests, TestFirstMovie)
+{
+    DatasetAccessor myDataset = DatasetAccessor();
+    
+    vector<Movie> generatedMovieVector = myDataset.generateMoviesVector();
+
+    EXPECT_EQ(generatedMovieVector.at(0).getName(), "The Shining");
+    EXPECT_EQ(generatedMovieVector.at(0).getStarringActor(), "Jack Nicholson");
+    EXPECT_EQ(generatedMovieVector.at(0).getGenre(), "Drama");
+    EXPECT_EQ(generatedMovieVector.at(0).getDirector(), "Stanley Kubrick");
+    EXPECT_EQ(generatedMovieVector.at(0).getTotalVotes(), 927000);
+    EXPECT_EQ(generatedMovieVector.at(0).getReleaseYear(), 1980);
+    EXPECT_EQ(generatedMovieVector.at(0).getRating(), 8.4);
+}
+
+TEST(MoviesVectorSetupTests, TestFiftiethMovie)
+{
+    DatasetAccessor myDataset = DatasetAccessor();
+    
+    vector<Movie> generatedMovieVector = myDataset.generateMoviesVector();
+
+    EXPECT_EQ(generatedMovieVector.at(49).getName(), "Lion of the Desert");
+    EXPECT_EQ(generatedMovieVector.at(49).getStarringActor(), "Anthony Quinn");
+    EXPECT_EQ(generatedMovieVector.at(49).getGenre(), "Biography");
+    EXPECT_EQ(generatedMovieVector.at(49).getDirector(), "Moustapha Akkad");
+    EXPECT_EQ(generatedMovieVector.at(49).getTotalVotes(), 15000);
+    EXPECT_EQ(generatedMovieVector.at(49).getReleaseYear(), 1980);
+    EXPECT_EQ(generatedMovieVector.at(49).getRating(), 8.3);
+}
+
+TEST(MoviesVectorSetupTests, Test2500thMovie)
+{
+    DatasetAccessor myDataset = DatasetAccessor();
+    
+    vector<Movie> generatedMovieVector = myDataset.generateMoviesVector();
+
+    EXPECT_EQ(generatedMovieVector.at(2499).getName(), "Only You");
+    EXPECT_EQ(generatedMovieVector.at(2499).getStarringActor(), "Marisa Tomei");
+    EXPECT_EQ(generatedMovieVector.at(2499).getGenre(), "Comedy");
+    EXPECT_EQ(generatedMovieVector.at(2499).getDirector(), "Norman Jewison");
+    EXPECT_EQ(generatedMovieVector.at(2499).getTotalVotes(), 21000);
+    EXPECT_EQ(generatedMovieVector.at(2499).getReleaseYear(), 1994);
+    EXPECT_EQ(generatedMovieVector.at(2499).getRating(), 6.5);
+}
+
+TEST(MoviesVectorSetupTests, TestNumMovies) {
+    DatasetAccessor myDataset = DatasetAccessor();
+    
+    vector<Movie> generatedMovieVector = myDataset.generateMoviesVector();
+
+    // check if all 7668 movies were passed into the vector
+    EXPECT_EQ(generatedMovieVector.size(), 7668);
+
+
+
+}
