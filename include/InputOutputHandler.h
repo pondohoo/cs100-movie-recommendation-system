981261eb8e06b224e79a3d7e5a13013632b889a2
theo <theobfernandez@gmail.com>
2022-11-24 19:50:23 -0800

    Improve sort/see next page of recommended movies main.cpp logic

--- include/InputOutputHandler.h
+++ include/InputOutputHandler.h
@@ -12,17 +12,17 @@ class InputOutputHandler {
         // handles invalid input and returns valid user input
         int handleIntroOptions();
         int handleSortOptions();
+        int handleSpecificMovieOptions();
+        int handleEndMessage();
+        int handleFinalDecision();
 
         void printRecommendedMoviesHeader();
-        int handleSpecificMovieOptions();
-        void printEndMessage();
         void printSortOptionHeader(int option);
 
         string handleNameRecommendationOption(Movies& movieRecommendationInterface);
         string handleGenreRecommendationOption(Movies& movieRecommendationInterface);
         string handleActorRecommendationOption(Movies& movieRecommendationInterface);
         string handleDirectorRecommendationOption(Movies& movieRecommendationInterface);
-        void handleRecommendedMoviesNextPage(Movies& movieRecommendationInterface);
         
 
 
